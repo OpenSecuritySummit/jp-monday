704152896421e725e9da679b843984dc1622fb11
UAULZ1T98 <gsbot@photobox.com>
2019-06-05 06:43:30 +0000

    auto saved (changes in notebook)

--- notebooks/api/oss_bot_utils.py
+++ notebooks/api/oss_bot_utils.py
@@ -4,6 +4,12 @@ def ping():
 class ossbot_utils:
     pass
 
-def show_png(png_data,height=200):
-    html = '<img style="border:1px solid black;height:{0}pt;" align="left" src="data:image/png;base64,{1}"/>'.format(height,png_data)
-    display_html(html, raw=True)    
+def render_example():
+    aws_lambda = Lambda('osbot_browser.lambdas.lambda_browser')
+    payload = {"params": ["render", "examples/wardley_map/cup-of-tea.html"],
+               'data': {}}
+    png_data = aws_lambda.invoke(payload)
+    show_png(png_data)
+    def show_png(png_data,height=200):
+        html = '<img style="border:1px solid black;height:{0}pt;" align="left" src="data:image/png;base64,{1}"/>'.format(height,png_data)
+        display_html(html, raw=True)    
