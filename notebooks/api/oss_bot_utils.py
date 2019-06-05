250ffe65b9928027ac853bbc6d8275f0652dbae4
UAULZ1T98 <gsbot@photobox.com>
2019-06-05 06:39:24 +0000

    auto saved (changes in notebook)

--- notebooks/api/oss_bot_utils.py
+++ notebooks/api/oss_bot_utils.py
@@ -3,3 +3,7 @@ def ping():
 
 class ossbot_utils:
     pass
+
+def show_png(png_data,height=200):
+    html = '<img style="border:1px solid black;height:{0}pt;" align="left" src="data:image/png;base64,{1}"/>'.format(height,png_data)
+    display_html(html, raw=True)    
