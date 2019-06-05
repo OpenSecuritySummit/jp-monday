0bb3a26da02c43d5b2d7805e5a6ef1bffa4b38be
UAULZ1T98 <gsbot@photobox.com>
2019-06-05 07:04:57 +0000

    auto saved (changes in notebook)

--- notebooks/api/oss_bot_utils.py
+++ notebooks/api/oss_bot_utils.py
@@ -7,9 +7,10 @@ def ping():
 class ossbot_utils:
     pass
 
-def render_example():
+def render_example(name):
+    file = "examples/wardley_map/{0}.html".format(name)
     aws_lambda = Lambda('osbot_browser.lambdas.lambda_browser')
-    payload = {"params": ["render", "examples/wardley_map/cup-of-tea.html"],
+    payload = {"params": ["render", ],
                'data': {}}
     png_data = aws_lambda.invoke(payload)
     show_png(png_data)
