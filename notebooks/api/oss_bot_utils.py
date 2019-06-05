1f3ce4292f1e5632a3b8b04185aca02c739df4cb
UAULZ1T98 <gsbot@photobox.com>
2019-06-05 06:44:54 +0000

    auto saved (changes in notebook)

--- notebooks/api/oss_bot_utils.py
+++ notebooks/api/oss_bot_utils.py
@@ -10,6 +10,6 @@ def render_example():
                'data': {}}
     png_data = aws_lambda.invoke(payload)
     show_png(png_data)
-    def show_png(png_data,height=200):
-        html = '<img style="border:1px solid black;height:{0}pt;" align="left" src="data:image/png;base64,{1}"/>'.format(height,png_data)
-        display_html(html, raw=True)    
+def show_png(png_data,height=200):
+    html = '<img style="border:1px solid black;height:{0}pt;" align="left" src="data:image/png;base64,{1}"/>'.format(height,png_data)
+    display_html(html, raw=True)    
