cb9ff902a7c98daebbc68654901876bc28cd4c28
UAULZ1T98 <gsbot@photobox.com>
2019-06-06 07:07:22 +0000

    auto saved (changes in notebook)

--- notebooks/users/dinis/vis-js/API-Vis_JS.js
+++ notebooks/users/dinis/vis-js/API-Vis_JS.js
@@ -1,19 +1,19 @@
  // create an array with nodes
   var nodes = new vis.DataSet([
-    {id: 1, label: 'Node 1', shape: 'box'  , fixed: true , x:0, y:0 },
-    {id: 2, label: 'Node 2', color: 'blue' , fixed: true , x:-100, y:0},
-    {id: 3, label: 'Node 3'},
-    {id: 4, label: 'Node 4'},
-    {id: 5, label: 'Node 5'}
+//     {id: 1, label: 'Node 1', shape: 'box'  , fixed: true , x:0, y:0 },
+//     {id: 2, label: 'Node 2', color: 'blue' , fixed: true , x:-100, y:0},
+//     {id: 3, label: 'Node 3'},
+//     {id: 4, label: 'Node 4'},
+//     {id: 5, label: 'Node 5'}
   ]);
 
   // create an array with edges
   var edges = new vis.DataSet([
-    {from: 1, to: 3},
-    {from: 1, to: 2},
-    {from: 2, to: 4},
-    {from: 2, to: 5},
-    {from: 3, to: 3}
+//     {from: 1, to: 3},
+//     {from: 1, to: 2},
+//     {from: 2, to: 4},
+//     {from: 2, to: 5},
+//     {from: 3, to: 3}
   ]);
 
   // create a network
