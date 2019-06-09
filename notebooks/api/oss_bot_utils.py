54e145bd1e50e5d4adbb646968708a7fb892133c
UAULZ1T98 <gsbot@photobox.com>
2019-06-09 17:38:20 +0000

    auto saved (changes in notebook)

--- notebooks/api/oss_bot_utils.py
+++ notebooks/api/oss_bot_utils.py
@@ -24,5 +24,5 @@ def create_map(code, view="exec_js"):
     payload = {"params": ["maps",'create', code ],
                'data': {}}
     png_data = aws_lambda.invoke(payload)
-    png_data
+    return png_data
     
