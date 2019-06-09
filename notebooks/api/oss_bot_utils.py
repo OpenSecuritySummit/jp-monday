513430a7b1ef5d2df0785c9efd9c350cbe31d5c5
UAULZ1T98 <gsbot@photobox.com>
2019-06-09 17:44:05 +0000

    auto saved (changes in notebook)

--- notebooks/api/oss_bot_utils.py
+++ notebooks/api/oss_bot_utils.py
@@ -19,12 +19,14 @@ def show_png(png_data,height=200):
     html = '<img style="border:1px solid black;height:{0}pt;" align="left" src="data:image/png;base64,{1}"/>'.format(height,png_data)
     display_html(html, raw=True)    
     
-def create_map(code):
+def maps_create(code):
     aws_lambda = Lambda('osbot_browser.lambdas.lambda_browser')
     params = ["maps", "exec_js"]
     params.extend(code.split(' '))
+
     payload = {"params": params,
                'data': {}}
     png_data = aws_lambda.invoke(payload)
     show_png(png_data)
+
     
