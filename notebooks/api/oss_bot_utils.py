19804f05712f7a0b8bd925ff0189c73793765d20
UAULZ1T98 <gsbot@photobox.com>
2019-08-05 15:32:23 +0000

    auto saved (changes in notebook)

--- notebooks/api/oss_bot_utils.py
+++ notebooks/api/oss_bot_utils.py
@@ -15,6 +15,12 @@ def render_example(name):
     png_data = aws_lambda.invoke(payload)
     show_png(png_data)
     
+def show_graph(graph, height=200):
+    puml = graph.render_puml().puml
+    result = Lambda('utils.puml_to_png').invoke({"puml": puml})
+    png_data = result.get('png_base64')
+    show_png(png_data, height)
+    
 def show_png(png_data,height=200):
     html = '<img style="border:1px solid black;height:{0}pt;" align="left" src="data:image/png;base64,{1}"/>'.format(height,png_data)
     display_html(html, raw=True)    
