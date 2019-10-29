efd90692f989df690821816ad39a5cd6f52cbf1f
UPRRX76RE <gsbot@photobox.com>
2019-10-29 14:34:34 +0000

    auto saved (changes in notebook)

--- notebooks/users/Rachel/debug/JiraWrangler.py
+++ notebooks/users/Rachel/debug/JiraWrangler.py
@@ -7,6 +7,7 @@ import json
 class JiraIssueWrangler():
     
     def __init__(self, issue_id):
+        self.issue = {}
         self.issue_id = issue_id
         self.api_jira = API_Jira()
         self.api_jira._jira = JIRA(server='https://ubuntu-policy.atlassian.net/')
@@ -14,6 +15,25 @@ class JiraIssueWrangler():
     def _get_issue(self, issue_id):
         return self.api_jira.issue(issue_id)
                     
+    def _get_related_issues(self, issue_id):
+        return issue
+        
     def wrangle(self):
-        base_issue = self._get_issue(self.issue_id)             
+        issue = self._get_issue(self.issue_id)
+        
+        for link_id in issue_links:
+            for issue_link in issue_links[link_id]:
+                if issue_link['Link Type'] == self.link_type:
+        
         return json.dumps(base_issue)
+
+
+class Issue():
+    
+    def __init__(self, key, summary, status, related_issues):
+        self.key = key
+        self.summary = summary
+        self.status = status
+        self.related_issues = related_issues
+
+        
