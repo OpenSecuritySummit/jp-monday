af8369a67a783f665c908933273b5ee5ed02da9a
UPRRX76RE <gsbot@photobox.com>
2019-10-29 14:36:07 +0000

    auto saved (changes in notebook)

--- notebooks/users/Rachel/debug/JiraWrangler.py
+++ notebooks/users/Rachel/debug/JiraWrangler.py
@@ -4,10 +4,19 @@ from osbot_jira.api.jira_server.API_Jira import API_Jira
 import json
 
     
+
+class Issue():
+    
+    def __init__(self, key, summary, status, related_issues):
+        self.key = key
+        self.summary = summary
+        self.status = status
+        self.related_issues = related_issues
+
+
 class JiraIssueWrangler():
     
     def __init__(self, issue_id):
-        self.issue = {}
         self.issue_id = issue_id
         self.api_jira = API_Jira()
         self.api_jira._jira = JIRA(server='https://ubuntu-policy.atlassian.net/')
@@ -19,7 +28,16 @@ class JiraIssueWrangler():
         return issue
         
     def wrangle(self):
-        issue = self._get_issue(self.issue_id)
+        issue_response = self._get_issue(self.issue_id)
+        
+        issue = Issue(
+            issue_response['Key'],
+            issue_response['Summary'],
+            issue_response['Status'],
+            issue_response['Related Issues'],
+        )
+        
+        issue = 
         
         for link_id in issue_links:
             for issue_link in issue_links[link_id]:
@@ -28,12 +46,5 @@ class JiraIssueWrangler():
         return json.dumps(base_issue)
 
 
-class Issue():
-    
-    def __init__(self, key, summary, status, related_issues):
-        self.key = key
-        self.summary = summary
-        self.status = status
-        self.related_issues = related_issues
 
         
