c93f3e32b99680183cbea27d7f1934f706e237c4
UPRRX76RE <gsbot@photobox.com>
2019-10-29 14:45:51 +0000

    auto saved (changes in notebook)

--- notebooks/users/Rachel/debug/JiraWrangler.py
+++ notebooks/users/Rachel/debug/JiraWrangler.py
@@ -29,22 +29,26 @@ class JiraIssueWrangler():
         self.api_jira = API_Jira()
         self.api_jira._jira = JIRA(server='https://ubuntu-policy.atlassian.net/')
         
-    def _get_issue(self, issue_id):
-        return self.api_jira.issue(issue_id)
-                    
-    def _get_related_issues(self, issue_id):
-        return issue
-        
-    def wrangle(self):
-        issue_response = self._get_issue(self.issue_id)
-        
-        issue = Issue(
-            issue_response['Key'],
-            issue_response['Summary'],
-            issue_response['Status'],
-            issue_response['Related Issues'],
+    def _get_issue(self, issue_response):
+        return Issue(
+            response['Key'],
+            response['Summary'],
+            response['Status'],
+            self._get_related_issues(response)
         )
+    
+    def _get_related_issues(self, issue_response):
+        related_issues = []
         
+        for key, related_issue in issue_response['Issue Links'].items()
+            related_issue_response = self.api_jira.issue(key)
+            related_issues.append(self._get_issue(related_issue_response)
+            
+        return related_issues
+                    
+    def wrangle(self):
+        base_issue_response = self.api_jira.issue(issue_id)
+        issue = self._get_issue(base_issue_response)
         return issue.toJSON();
 
 
